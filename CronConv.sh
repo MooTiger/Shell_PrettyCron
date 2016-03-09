487d081459dc394a303b785ce7e0f6390c346680
MooTiger <meowtiger@gmail.com>
2016-03-09 13:36:08 +0000

    Create CronConv.sh
    After searching for a way to do this i could only find Python or PHP methods .. 
    I needed comma separated days / times and the python version https://github.com/adamchainz/pretty-cron didn't work for me (python2 in RHEL)
    
    I sat down and wrote it in shell .. This is the first working version and could be neater / cleaner.

---
+++ CronConv.sh
@@ -0,0 +1,108 @@
+#specify cronfile to read
+cronfile=/var/spool/cron/root
+
+
+function Pretty_Time {
+prettymin=
+prettyhour=
+prettytime=
+
+if [[ ${min} = "*" ]]; then
+	prettymin="Every Minute"
+elif [[ ${min} -eq 0 ]]; then 
+	prettymin="00"
+else
+	prettymin=${min}
+fi
+
+if [[ ${hour} = "*" ]]; then
+	prettyhour="of Every Hour"
+elif [[ ${hour} -eq 0 ]]; then 
+	prettyhour="00"
+else
+	prettyhour=${hour}
+fi
+
+if [[ $(echo ${hour} | awk '/,/') ]] && [[ $(echo ${min} | awk '/,/') ]]; then
+	for i in $(echo ${hour} | tr ',' ' ');do
+		for m in $(echo ${min} | tr ',' ' '); do
+			temptime="${i}:${m}"
+			prettytime="${prettytime} ${temptime}"
+		done
+#	temphour="${temphour}:${prettymin} ${i}"
+#	temphour=$(echo ${temphour} | sed 's/^:..//')
+	done
+	prettymin=
+	prettyhour=
+elif [[ $(echo ${hour} | awk '/,/') ]]; then
+	for i in $(echo ${hour} | tr ',' ' ');do
+		temphour="${temphour}:${prettymin} ${i}"
+		temphour=$(echo ${temphour} | sed 's/^:..//')
+	done
+	prettytime="${temphour}:${prettymin}"
+	prettymin=
+	prettyhour=
+elif [[ $(echo ${min} | awk '/,/') ]]; then
+	for i in $(echo ${min} | tr ',' ' '); do
+		tempmin="${prettyhour}:${i}"
+		prettytime="${prettytime} ${tempmin}"
+	done
+	prettymin=
+	prettyhour=
+fi	
+}
+
+function Pretty_DoM {
+prettydom=
+if [[ ${DoM} = "*" ]]; then
+	prettydom="and Every Day"
+else 
+	prettydom="on the ${DoM} of"
+fi
+}
+
+function Pretty_Month {
+prettymonth=
+MONTHS=(ZERO Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+if [[ ${Month} = "*" ]]; then
+	prettymonth="of Every Month"
+elif [[ $(echo ${Month} | awk '/,/') ]] ; then
+	for i in $(echo ${Month} | tr ',' ' ') ; do
+		tempmonth="${tempmonth} ${MONTHS[${i}]}"
+	done
+	prettymonth="In ${tempmonth}"
+else
+	prettymonth="In ${MONTHS[${Month}]}"
+fi
+}
+
+function Pretty_DoW {
+prettydow=
+DAYS=(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+if [[ ${DoW} = "*" ]]; then
+	prettydow="Every Weekday"
+elif [[ $(echo ${DoW} | awk '/,/') ]] ; then
+	for i in $(echo ${DoW} | tr ',' ' ') ; do
+		tempdow="${tempdow} ${DAYS[${i}]}"
+	done
+	prettydow="On the weekdays${tempdow}"
+else
+	prettydow="On ${DAYS[${DoW}]}"
+fi
+}
+
+#Start of while read loop
+cat ${cronfile} | while read min hour DoM Month DoW CMD; do 
+	export crontime=$(printf "%s %s %s %s %s\n" "$min" "$hour" "$DoM" "$Month" "$DoW") 
+
+
+	Pretty_Time
+	Pretty_DoM
+	Pretty_Month
+	Pretty_DoW
+
+
+	echo "CronTime is ${crontime}" #Just to see what the input fields were
+	echo "${prettytime}${prettyhour} ${prettymin} ${prettydow} ${prettydom} ${prettymonth}"
+
+done #End of While Read Loop
